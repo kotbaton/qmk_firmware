4ab640d600d712683b67aaa1a0e7adb96ad7c95f
Andrii Shekhovtsov <zzzadey@gmail.com>
2022-09-24 19:57:38 +0200

    Change pic on crkbd

--- keyboards/crkbd/keymaps/kotbaton/config.h
+++ keyboards/crkbd/keymaps/kotbaton/config.h
@@ -94,4 +94,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
-#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+//#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+#define OLED_FONT_H "keyboards/crkbd/keymaps/kotbaton/font.c"
